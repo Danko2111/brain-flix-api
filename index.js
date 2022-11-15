3cd940353324f2a80ae3d6a03cec5732008866b7
Danko2111 <82421874+Danko2111@users.noreply.github.com>
2022-11-14 20:46:41 -0800

    initial commit

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const data = require("./data/videos.json");
 const fs = require("fs");
 const express = require("express");
 const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 app.use(cors());
 
 // express get method used to respond with json data of the videos arr
-app.get("/videos", (req, res) => {
-  res.json(
-    data.map(({ id, title, channel, image }) => {
-      return { id, title, channel, image };
-    })
-  );
-});
+app
+  .route(`/videos`)
+  .get((req, res) => {
+    res.json(
+      data.map(({ id, title, channel, image }) => {
+        return { id, title, channel, image };
+      })
+    );
+  })
+  .post((req, res) => {
+    fs.writeFile();
+  });
 // express get method used to respond with the json data of the specific video that is requested using the url params
 app.get("/videos/:id", (req, res) => {
   res.json(
