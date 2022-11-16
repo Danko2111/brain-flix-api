2a4d850670c7d7e052a656e2e55e6b6d47559c50
Daniel Kolesnitski <primehd2111@gmail.com>
2022-11-16 14:56:28 -0800

    added in comment delete functionality to the api

--- routes/vidoesRoutes.js
+++ routes/vidoesRoutes.js
@@ -3,20 +3,32 @@ const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const router = express.Router();
 
+// creating a reusable redfile func that takes in a callback func to deal with the return of data
+const readFileFunc = (readFileCallback) => {
+  fs.readFile("./data/videos.json", "utf8", readFileCallback);
+};
+
+const writeFileFunc = (fileToWrite) => {
+  fs.writeFile("./data/videos.json", JSON.stringify(fileToWrite), (err) => {
+    if (err) console.log(err);
+  });
+};
+
 router
   .route("/")
   //express get method used to respond with json data of the videos arr
-  .get((req, res) => {
-    fs.readFile("./data/videos.json", (err, data) => {
+  .get((_req, res) => {
+    readFileFunc((err, data) => {
       //catching errors and sending back a json error message
       if (err) {
         res.status(500).json({
-          message: "Something went wrong, please try again later",
+          message:
+            "Something went wrong while trying to read the file, please try again later",
           error: err,
         });
       } else {
         // responding with parsed json data
-        res.json(
+        res.status(201).json(
           JSON.parse(data).map(({ id, title, channel, image }) => {
             return { id, title, channel, image };
           })
@@ -27,7 +39,7 @@ router
   //express post method used to update the json with the new incomming video obj
   .post((req, res) => {
     // reading the json
-    fs.readFile("./data/videos.json", (err, data) => {
+    readFileFunc((err, data) => {
       //throwing an error if there is an issue reading the file
       if (err) {
         res.status(500).json({
@@ -57,14 +69,11 @@ router
         // pushing the new video obj into our array
         newVideosArr.push(newVideo);
         // writing the new array into json data and overriding the old json data
-        fs.writeFile(
-          "./data/videos.json",
-          JSON.stringify(newVideosArr),
-          (err) => {
-            if (err) console.log(err);
-          }
-        );
-        res.status(200).json(newVideo);
+        writeFileFunc(newVideosArr);
+        res.status(200).json({
+          message: "Your video successfully was posted",
+          video: newVideo,
+        });
         // catching errors with the incoming request body
       } else {
         res.status(500).json({
@@ -78,14 +87,17 @@ router
 
 // express get method used to respond with the json data of the specific video that is requested using the url params
 router.get("/:id", (req, res) => {
-  fs.readFile("./data/videos.json", (err, data) => {
+  // reading the json file
+  readFileFunc((err, data) => {
     if (err) {
       res.json({
-        message: "Something went wrong, please try again",
+        message:
+          "Something went wrong while trying to read the file, please try again later",
         error: err,
       });
     } else {
-      res.json(
+      // responding with the specific video details that match the req param id
+      res.status(201).json(
         JSON.parse(data).find((vid) => {
           return vid.id === req.params.id;
         })
@@ -95,7 +107,7 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/:id/comments", (req, res) => {
-  fs.readFile("./data/videos.json", (err, data) => {
+  readFileFunc((err, data) => {
     // throwing an error if there is an issue reading the file
     if (err) {
       res.status(500).json({
@@ -115,18 +127,14 @@ router.post("/:id/comments", (req, res) => {
         likes: 0,
         timestamp: Date.now(),
       };
+      //getting specific video that matches the req params id
       const videoDetails = newVideosArr.find((vid) => {
         return vid.id === req.params.id;
       });
+      // pushing the new comment obj into that video comments array
       videoDetails.comments.push(newComment);
-      fs.writeFile(
-        "./data/videos.json",
-        JSON.stringify(newVideosArr),
-        (err) => {
-          if (err) console.log(err);
-        }
-      );
-      res.json({ message: "Your comment posted successfully" });
+      writeFileFunc(newVideosArr);
+      res.status(200).json({ message: "Your comment posted sucessfully" });
     } else {
       res.status(500).json({
         message:
@@ -136,5 +144,35 @@ router.post("/:id/comments", (req, res) => {
     }
   });
 });
-
+router.delete("/:videoId/comments/:commentsId", (req, res) => {
+  readFileFunc((err, data) => {
+    const videoId = req.params.videoId;
+    const commentId = req.params.commentsId;
+    if (err) {
+      res.status(500).json({
+        message:
+          "Something went wrong while trying to read the file, please try again later",
+        error: err,
+      });
+    }
+    newVideosArr = JSON.parse(data);
+    if (!newVideosArr.find((video) => video.id === videoId)) {
+      res.send("This video ID doesnt exist");
+    } else {
+      const selectedVideo = newVideosArr.find((video) => video.id === videoId);
+      if (!selectedVideo.comments.find((comment) => comment.id === commentId)) {
+        res.send("This comment ID doesnt exist");
+      } else {
+        const selectedCommentIndex = selectedVideo.comments.findIndex(
+          (comment) => comment.id === commentId
+        );
+        selectedVideo.comments.splice(selectedCommentIndex, 1);
+        writeFileFunc(newVideosArr);
+        res
+          .status(200)
+          .json({ message: "Your comment was sucessfully deleted" });
+      }
+    }
+  });
+});
 module.exports = router;
